3925b577a059cdf0abbe219ef227ef7cd754cf46
Douglas Monteiro <do.uglas.14@hotmail.com>
2023-08-18 23:00:53 -0300

    Calculadora em Javascript01~

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+function Calculadora(){
+
+this.inputCalc = document.querySelector('#input_calc');
+
+this.inicia = () => {
+    this.btn();
+}
+
+this.btn = () => {
+    document.addEventListener('click' , (event) => {
+        const element = event.target;
+        if(element.classList.contains('btn')) this.addNum(element);
+        if(element.classList.contains('btn-clear')) this.clearDisplay();
+        if(element.classList.contains('btn-apagar')) this.clearElement();
+        if(element.classList.contains('btn-igual')) this.calcular();
+    })
+}
+
+//metodos
+
+this.calcular = () =>{
+    try{
+        const conta = eval(this.inputCalc.value)
+        if(!conta){
+            alert('Conta invalida!!')
+            return;
+        }
+        this.inputCalc.value = conta; 
+    }catch (e){
+        alert('Conta invalida!!')
+        return;
+    }
+}
+
+this.addNum = (element) => {
+    this.inputCalc.value += element.innerText;
+}
+
+this.clearDisplay = () => {
+    this.inputCalc.value = '';
+}
+
+this.clearElement = () => {
+    this.inputCalc.value = this.inputCalc.value.slice(0, -1);
+}
+
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
+
+
+
